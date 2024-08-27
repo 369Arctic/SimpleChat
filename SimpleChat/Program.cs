fe4fec065324f7f74d185cfad9e97ace46dc90a5
Dmitrii Sannikov <omeom@mail.ru>
2024-08-27 22:09:41 +0500

    Simple chat with SignalR

---
+++ SimpleChat/Program.cs
@@ -0,0 +1,33 @@
+using SimpleChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
